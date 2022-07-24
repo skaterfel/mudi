55747a7e1872effc75d73fccf3d886a95d0a07d0
Felipe Melo <felipe.leandro2@gmail.com>
2022-07-24 12:43:02 -0300

    Usando Thymeleaf e BootStrap

---
+++ script-inicial.bat
@@ -0,0 +1,7 @@
+echo "# pedidos-exterior" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/skaterfel/pedidos-exterior.git
+git push -u origin main
