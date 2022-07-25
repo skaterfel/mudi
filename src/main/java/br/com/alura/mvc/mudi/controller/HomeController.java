28cb08990005c0f83261fd0e6f84f74661314ed0
Felipe Melo <felipe.leandro2@gmail.com>
2022-07-25 08:54:50 -0300

    Criação da classe de controler, repository, thymeleaf

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,30 +1,28 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+
 
 @Controller
 public class HomeController {
-	
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
 	@GetMapping("/home")
 	public String home(Model model) {
-		
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Celular Xiaomi Note 8");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51wgmCYDFML._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Celular-Xiaomi-Vers%C3%A3o-Global-Space/dp/B07Y9ZHLXW/ref=sr_1_1?keywords=xiaomi+redmi+note+8&qid=1658673595&sr=8-1&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147");
-		pedido.setDescricao("UMA DESCRIÇÃO QUALQUER PARA ESTE PEDIDO!!");
-		pedido.setValorNegociado(new BigDecimal(1200));
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos", pedidos);
-		
+
 		return "home";
 	}
+
 }
--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -3,14 +3,27 @@ package br.com.alura.mvc.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public class Pedido {
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Pedido {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
 	private LocalDate dataDaEntrega;
 	private String urlProduto;
 	private String urlImagem;
 	private String descricao;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusPedido status;
 	
 	public String getNomeProduto() {
 		return nomeProduto;
@@ -47,6 +60,12 @@ public class Pedido {
 	}
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+	public StatusPedido getStatus() {
+		return status;
+	}
+	public void setStatus(StatusPedido status) {
+		this.status = status;
 	}
 	
 }
